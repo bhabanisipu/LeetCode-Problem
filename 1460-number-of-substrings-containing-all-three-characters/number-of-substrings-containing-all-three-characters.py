cdd1e3bad6f319c3fafb8592987469556205ec56
Bhabani Sankar Sahoo <156910182+bhabanisipu@users.noreply.github.com>
2026-08-11 11:57:17 +0530

    Time: 112 ms (29.72%) | Memory: 19.4 MB (47.67%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
@@ -3,7 +3,6 @@ class Solution:
         n = len(s)
         cnt = 0
         left = 0
-        r = list(set(s))
         dic = {'a':0,'b':0,'c':0}
         for right in range(n):
             dic[s[right]] += 1
